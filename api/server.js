ee4d7f632a2cbce894f4d62a56cb66f757a63734
Kronykus <MoreheadSteve@gmail.com>
2019-12-16 18:08:19 -0600

    reclone

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require( 'express' );
+
+const apiRouter = require( './routers/router.js' );
+
+const middlewareConfig = require( './middlware-config.js' );
+
+const server = express();
+middlewareConfig( server );
+
+server.use( '/api/router', apiRouter );
+
+module.exports = server;
---
+++ data/models/userModel.js
@@ -0,0 +1,26 @@
+const db = require('../dbConfig.js');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+};
+
+function find() {
+  return db('users').select('id', 'username');
+}
+
+function add(user) {
+  return db('users')
+    .insert(user, 'id')
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findBy(filter) {
+  return db('users')
+    .select('id', 'username')
+    .where(filter);
+}
